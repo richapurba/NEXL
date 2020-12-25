0dd5cb7060b9bb7dc59bf6aede1417bae9bf5d19
Richa Purba <richathorne@gmail.com>
2020-12-26 06:35:10 +0700

    Best navbar

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ window.addEventListener("load", gone);
 function gone() {
   loader.classList.add("gone");
 }
+
+//navigation
+var $menu, $body, $pages;
+  $(document).ready(function() {
+    $menu = $(".menu a");
+    $body = $("body,html");
+    $pages = $(".pages > div");
+
+    $menu.click(function() {
+      var index = $menu.index(this);
+      var offset = $pages.eq(index).offset().top;
+
+      $body.animate({scrollTop : offset}, 500);
+
+      return false;
+    });
+    $(document).trigger("scroll");
+  });
+$(document).scroll(function() {
+  var index = Math.round($(this).scrollTop()/$(window).height());
+    $menu.removeClass("active");
+    $menu.eq(index).addClass("active");
+})
