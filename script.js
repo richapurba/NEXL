453a1f9180cdb18fe022baa05c0933f6d9b6a87e
Richa Purba <richathorne@gmail.com>
2020-12-27 19:02:33 +0700

    Add animation for the contact form

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ $(document).scroll(function() {
     $menu.removeClass("active");
     $menu.eq(index).addClass("active");
 })
+
+
+
+
+//contact form
+const inputs = document.querySelectorAll(".input");
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");}
+}
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
